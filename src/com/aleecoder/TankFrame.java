308d355283090b905147ad1981344de937f4a70b
HuanyuLee <hadoop_2020@163.com>
2022-07-31 22:15:58 +0800

    优化了paint方法的逻辑，利用双缓冲机制消除了坦克大战中景物的闪烁和白条，增加了子弹生命检查

--- src/com/aleecoder/TankFrame.java
+++ src/com/aleecoder/TankFrame.java
@@ -1,7 +1,12 @@
 package com.aleecoder;
 
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 坦克大战的窗口程序
@@ -9,14 +14,19 @@ import java.awt.event.*;
  * @date 2022/7/18
  */
 public class TankFrame extends Frame {
-    private Tank tank;
-    private Tank enemy;
+    private final Tank tank;
+    private final Tank enemy;
+    private List<Bullet> bullets;
 
-    public TankFrame() {
+    public static final int GAME_WIDTH = 1080, GAME_HEIGHT = 960;
+    public static final TankFrame INSTANCE = new TankFrame();
+
+    private TankFrame() {
         this.setTitle("tank-single");
         this.setLocation(400, 200);
         this.setSize(800, 600);
         this.setVisible(true);
+        this.setResizable(false);
         // 关闭窗口
         this.addWindowListener(new WindowAdapter() {
             @Override
@@ -27,8 +37,9 @@ public class TankFrame extends Frame {
         // TODO: 观察者 设计模式 Observer
         // 手动控制：响应键盘事件
         this.addKeyListener(new TankKeyListener());
-        tank = new Tank(100, 100, Direction.RIGHT);
-        enemy = new Tank(200, 200, Direction.DOWN);
+        tank = new Tank(100, 100, Dir.R, Group.GOOD);
+        enemy = new Tank(200, 200, Dir.D, Group.BAD);
+        bullets = new ArrayList<>();
     }
 
     /**
@@ -38,8 +49,54 @@ public class TankFrame extends Frame {
      */
     @Override
     public void paint(Graphics g) {
+        init(g);
+        // 子弹生命检查
+        bullets.removeIf(bullet -> !bullet.isLive());
+        for (Bullet bullet : bullets) {
+            bullet.collideWithTank(enemy);
+            bullet.paint(g);
+        }
+
+    }
+
+    /**
+     * paint初始化
+     * @param g 系统画笔
+     */
+    public void init(Graphics g) {
         tank.paint(g);
         enemy.paint(g);
+        // 保留现场
+        Color color = g.getColor();
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("JetBrains Mono", Font.BOLD, 12));
+        g.drawString("bullets: " + bullets.size(), 10, 50);
+        // 恢复现场
+        g.setColor(color);
+    }
+
+    Image offScreenImage = null;
+
+    /**
+     * 双缓冲机制，消除坦克大战中景物的闪烁和白条
+     * @param g 系统画笔
+     */
+    @Override
+    public void update(Graphics g) {
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage, 0, 0, null);
+    }
+
+    public void add(Bullet bullet) {
+        this.bullets.add(bullet);
     }
 
     /**
