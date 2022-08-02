18ab66d40233cc141865f189b3a1a67b37a61384
HuanyuLee <hadoop_2020@163.com>
2022-08-02 23:37:34 +0800

    new出多个敌军坦克并在上方显示，简化paint方法的逻辑

--- src/com/aleecoder/TankFrame.java
+++ src/com/aleecoder/TankFrame.java
@@ -14,17 +14,19 @@ import java.util.List;
  * @date 2022/7/18
  */
 public class TankFrame extends Frame {
-    private final Tank tank;
-    private final Tank enemy;
+    private Player tank;
+    private List<Tank> enemies;
     private List<Bullet> bullets;
 
-    public static final int GAME_WIDTH = 1080, GAME_HEIGHT = 960;
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    public final int GAME_WIDTH = (int) screenSize.getWidth();
+    public final int GAME_HEIGHT = (int) screenSize.getHeight();
     public static final TankFrame INSTANCE = new TankFrame();
 
     private TankFrame() {
         this.setTitle("tank-single");
-        this.setLocation(400, 200);
-        this.setSize(800, 600);
+        this.setLocation(0, 0);
+        this.setSize(GAME_WIDTH, GAME_HEIGHT);
         this.setVisible(true);
         this.setResizable(false);
         // 关闭窗口
@@ -37,9 +39,16 @@ public class TankFrame extends Frame {
         // TODO: 观察者 设计模式 Observer
         // 手动控制：响应键盘事件
         this.addKeyListener(new TankKeyListener());
-        tank = new Tank(100, 100, Dir.R, Group.GOOD);
-        enemy = new Tank(200, 200, Dir.D, Group.BAD);
+        initGameObjects();
+    }
+
+    private void initGameObjects() {
+        tank = new Player(GAME_WIDTH / 2, GAME_HEIGHT - ResourceMgr.TANK_HEIGHT, Dir.U, Group.GOOD);
         bullets = new ArrayList<>();
+        enemies = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            enemies.add(new Tank(GAME_WIDTH / 3 + ResourceMgr.TANK_WIDTH * i, 30, Dir.D, Group.BAD));
+        }
     }
 
     /**
@@ -49,30 +58,44 @@ public class TankFrame extends Frame {
      */
     @Override
     public void paint(Graphics g) {
-        init(g);
+        initTank(g);
+        setCount(g);
         // 子弹生命检查
         bullets.removeIf(bullet -> !bullet.isLive());
         for (Bullet bullet : bullets) {
-            bullet.collideWithTank(enemy);
+            for (Tank enemy : enemies) {
+                bullet.collideWithTank(enemy);
+            }
             bullet.paint(g);
         }
 
     }
 
     /**
-     * paint初始化
+     * 子弹、敌军坦克计数
      * @param g 系统画笔
      */
-    public void init(Graphics g) {
-        tank.paint(g);
-        enemy.paint(g);
+    private void setCount(Graphics g) {
         // 保留现场
         Color color = g.getColor();
         g.setColor(Color.WHITE);
         g.setFont(new Font("JetBrains Mono", Font.BOLD, 12));
         g.drawString("bullets: " + bullets.size(), 10, 50);
+        g.drawString("enemies: " + enemies.size(), 10, 70);
         // 恢复现场
         g.setColor(color);
+    }
+
+    /**
+     * paint初始化坦克
+     * @param g 系统画笔
+     */
+    public void initTank(Graphics g) {
+        tank.paint(g);
+        enemies.removeIf(enemy -> !enemy.isLive());
+        for (Tank enemy : enemies) {
+            enemy.paint(g);
+        }
     }
 
     Image offScreenImage = null;
@@ -95,8 +118,20 @@ public class TankFrame extends Frame {
         g.drawImage(offScreenImage, 0, 0, null);
     }
 
+    /**
+     * 添加子弹
+     * @param bullet 子弹对象
+     */
     public void add(Bullet bullet) {
         this.bullets.add(bullet);
+    }
+
+    public int getGAME_WIDTH() {
+        return GAME_WIDTH;
+    }
+
+    public int getGAME_HEIGHT() {
+        return GAME_HEIGHT;
     }
 
     /**
