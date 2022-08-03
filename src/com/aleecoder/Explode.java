93408ec87c9e15b598237fc00d76ecbadb3f76ad
HuanyuLee <hadoop_2020@163.com>
2022-08-04 00:43:17 +0800

    增加爆炸类

---
+++ src/com/aleecoder/Explode.java
@@ -0,0 +1,41 @@
+package com.aleecoder;
+
+import java.awt.*;
+
+/**
+ * 爆炸类
+ * @author HuanyuLee
+ * @date 2022/8/3
+ */
+public class Explode {
+    private int x, y;
+    private int width, height;
+    private int step = 0;
+    private boolean live = true;
+
+    public Explode(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.width = ResourceMgr.explodes[0].getWidth();
+        this.height = ResourceMgr.explodes[0].getHeight();
+    }
+
+    public boolean isLive() {
+        return live;
+    }
+
+    public void paint(Graphics g) {
+        if (!live){
+            return;
+        }
+        // 一帧一帧的显示爆炸效果
+        g.drawImage(ResourceMgr.explodes[step++], x, y, null);
+        if (step >= ResourceMgr.explodes.length){
+            this.over();
+        }
+    }
+
+    private void over() {
+        live = false;
+    }
+}
