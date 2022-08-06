f98770c38e6ba1e4863a60a3ff7e0a3728773e11
HuanyuLee <hadoop_2020@163.com>
2022-08-06 11:29:48 +0800

    调用爆炸的声音特效

--- src/com/aleecoder/view/Explode.java
+++ src/com/aleecoder/view/Explode.java
@@ -21,8 +21,7 @@ public class Explode {
         this.y = y;
         this.width = ResourceMgr.explodes[0].getWidth();
         this.height = ResourceMgr.explodes[0].getHeight();
-        // 爆炸声音
-        new Thread(()-> new Audio("audio/explode.wav").play()).start();
+        Audio.explode();
     }
 
     public boolean isLive() {
