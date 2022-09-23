01766dc796d5f79908fe12d2ca95a35d13c164d8
HuanyuLee <hadoop_2020@163.com>
2022-09-23 20:51:19 +0800

    坦克大战网络版初始化

---
+++ src/main/java/com/aleecoder/Main.java
@@ -0,0 +1,11 @@
+package com.aleecoder;
+
+/**
+ * @author HuanyuLee
+ * @date 2022/8/13
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("net");
+    }
+}
